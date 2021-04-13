286173b60e2f9948a759053ac1a2dec52e9221d9
daipengpeng <16602124116@163.com>
2021-04-14 06:28:06 +0800

    project init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>11</div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+  props: {
+    msg: String,
+  },
+};
+</script>
---
+++ src/components/header/header.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>11</div>
+</template>
+
+<script>
+export default {
+  name: "MusicHeader",
+  data() {
+    return {};
+  },
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
