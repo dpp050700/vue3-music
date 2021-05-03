6ea8c6e34000e5ce155a46b6d6050b3d28b300b6
daipengpeng <16602124116@163.com>
2021-05-03 22:53:40 +0800

    歌手详情页面

--- src/views/singer-detail.vue
+++ src/views/singer-detail.vue
@@ -1,15 +1,24 @@
 <template>
   <div class="singer-detail">
-    <div class="bg-img" :style="bgImage" ref="bgImage"></div>
-    <scroll
-      class="list"
-      :style="scrollStyle"
-      ref="scrollRef"
-      :probe-type="3"
-      @pull-up="pullUp"
-    >
-      <song-list :list="list"></song-list>
-    </scroll>
+    <div class="music-list">
+      <div class="header">
+        <i class="music-icon-back"></i>
+        <h1>{{ detail.name }}</h1>
+      </div>
+      <div class="bg-img" :style="bgImage" ref="bgImage">
+        <div class="filter"></div>
+      </div>
+      <scroll
+        class="list"
+        :style="scrollStyle"
+        ref="scrollRef"
+        :probe-type="3"
+        @pull-up="pullUp"
+        @scroll="onScrollHandler"
+      >
+        <song-list :list="list"></song-list>
+      </scroll>
+    </div>
   </div>
 </template>
 
@@ -19,6 +28,8 @@ import { getSingerDetail, getSingerSongs } from "@/request/singers";
 import Scroll from "@/components/base/scroll/scroll.vue";
 import SongList from "@/components/song-list/song-list.vue";
 import usePagination from "@/hooks/use-pagination.js";
+
+const RESERVED_HEIGHT = 40;
 export default {
   name: "",
   components: {
@@ -29,6 +40,7 @@ export default {
     return {
       detail: {},
       imageHeight: 0,
+      scrollY: 0,
     };
   },
   async created() {
@@ -38,8 +50,25 @@ export default {
   },
   computed: {
     bgImage() {
+      const scrollY = this.scrollY;
+      let zIndex = 0;
+      let paddingTop = "70%";
+
+      let scale = 1;
+      if (scrollY > 0) {
+        scale = 1 + Math.abs(scrollY / this.imageHeight);
+      }
+
+      if (-scrollY > this.maxTranslateY) {
+        zIndex = 10;
+        paddingTop = `${RESERVED_HEIGHT}px`;
+      }
+
       return {
+        zIndex,
+        paddingTop,
         backgroundImage: `url("${this.detail.cover}")`,
+        transform: `scale(${scale})translateZ(0px)`,
       };
     },
     scrollStyle() {
@@ -52,7 +81,7 @@ export default {
 
   mounted() {
     this.imageHeight = this.$refs.bgImage.clientHeight;
-    console.log(this.imageHeight);
+    this.maxTranslateY = this.imageHeight - RESERVED_HEIGHT;
   },
   methods: {
     async singerDetail() {
@@ -60,6 +89,9 @@ export default {
     },
     async pullUp() {
       await this.nextPage({ id: this.id });
+    },
+    onScrollHandler(pos) {
+      this.scrollY = pos.y;
     },
   },
   setup(props) {
@@ -88,14 +120,44 @@ export default {
   left: 0;
   top: 0;
   background: #fff;
+  .music-list {
+    position: relative;
+    height: 100%;
+  }
   .bg-img {
     padding-top: 70%;
     background-size: cover;
+    position: relative;
+    height: 0;
+    transform-origin: top;
+    .filter {
+      position: absolute;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(7, 17, 27, 0.4);
+    }
   }
   .list {
     position: fixed;
     width: 100%;
     left: 0;
+  }
+  .header {
+    position: absolute;
+    height: 40px;
+    top: 0;
+    width: 100%;
+    text-align: center;
+    line-height: 40px;
+    color: #fff;
+    z-index: 20;
+    i {
+      position: absolute;
+      left: 10px;
+      font-size: 28px;
+    }
   }
 }
 </style>
